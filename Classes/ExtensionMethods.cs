43ae4e1c9269011502e8832d64819ba151fe9888
philipsaad <philipsaad@gmail.com>
2015-07-03 16:00:11 -0700

    Added basic argument handling and usage information

---
+++ Classes/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public static class ExtensionMethods
+{
+    public static List<string> Parse(this string[] args)
+    {
+        List<string> argList = new List<string>();
+
+        foreach (string arg in args)
+        {
+            argList.Add(arg.ToUpper());
+        }
+
+        return argList;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,17 @@ namespace VevoToTS
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Retrieve the arguments being passed into the application
+            List<string> args = Environment.GetCommandLineArgs().Parse();
+
+            //Show usage if the user did not pass any arguments or specifically passed in the usage argument
+            if ((args.Count == 1) || (args.Contains("/?"))) { ArgumentHandler.showUsage(); return; }
+
+            //Show version information if the user passed in the correct argument
+            if (args.Contains("/V")) { ArgumentHandler.showVersion(); return; }
+            
             VevoRendition vevoRendition = Vevo.GetBestVevoRendition("USUV71501692");
 
             if (vevoRendition.Url.EndsWith("m3u8"))

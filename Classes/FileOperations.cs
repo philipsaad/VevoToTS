31a127894daf0ee61797f30daac2c25651f2bd55
philipsaad <philipsaad@gmail.com>
2015-07-04 21:41:03 -0700

    Code cleanup, added Fody library and merged all third party assembies into main assembly for true static linking.

---
+++ Classes/FileOperations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace VevoToTS
+{
+    public static class FileOperations
+    {
+        public static List<string> ConcatenateFiles(List<string> transportStreamFiles, string outputFile)
+        {
+            using (Stream outputStream = File.OpenWrite(outputFile))
+            {
+                foreach (string transportStreamFile in transportStreamFiles)
+                {
+                    using (Stream transportStream = File.OpenRead(transportStreamFile))
+                    {
+                        transportStream.CopyTo(outputStream);
+                    }
+                }
+            }
+
+            return transportStreamFiles;
+        }
+
+        public static List<string> DownloadFiles(List<string> transportStreamFiles)
+        {
+            List<string> downloadedTransportStreamFiles = new List<string>();
+
+            using (var webClient = new WebClient())
+            {
+                int count = 1;
+                decimal total = transportStreamFiles.Count;
+
+                foreach (string transportStreamFile in transportStreamFiles)
+                {
+                    string percent = (count / total).ToString("p");
+                    Console.Write("\r{0}", "".PadRight(60, ' '));
+                    Console.Write("\rDownloading file " + count + " of " + total + ". (" + percent + ")");
+
+                    string tempFile = Path.GetTempFileName();
+                    webClient.DownloadFile(transportStreamFile, tempFile);
+                    downloadedTransportStreamFiles.Add(tempFile);
+
+                    count++;
+                }
+            }
+
+            return downloadedTransportStreamFiles;
+        }
+
+        public static void DeleteFiles(List<string> transportStreamFiles)
+        {
+            //Use PLINQ to loop through each file using a parallel loop
+            Parallel.ForEach(transportStreamFiles, fileToDelete =>
+            {
+                //Delete the file
+                File.Delete(fileToDelete);
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
 
 namespace VevoToTS
 {
@@ -19,63 +17,10 @@ namespace VevoToTS
 
             if (vevoRendition.Url.EndsWith("m3u8"))
             {
-                VevoHttpStreamingUrl bestVevoHttpStreamingUrl = Vevo.GetBestVevoHttpStreamingUrl(vevoRendition.Url);
-                List<string> transportFileList = Vevo.GetVevoTransportFileList(bestVevoHttpStreamingUrl.Url);
-                
-                deleteFiles(concatenateFiles(downloadFiles(transportFileList), options.OutputFile ?? vevoRendition.FileName));
+                FileOperations.DeleteFiles(FileOperations.ConcatenateFiles(FileOperations.DownloadFiles(Vevo.GetVevoTransportFileList(Vevo.GetBestVevoHttpStreamingUrl(vevoRendition.Url).Url)), options.OutputFile ?? vevoRendition.FileName));
 
                 Console.Write("\r{0}", "".PadRight(60, ' '));
                 Console.WriteLine("\rFile saved as: \"" + (options.OutputFile ?? vevoRendition.FileName) + "\"");
-            }
-        }
-
-        static List<string> concatenateFiles(List<string> transportStreamFiles, string outputFile)
-        {
-            using (Stream outputStream = File.OpenWrite(outputFile))
-            {
-                foreach (string transportStreamFile in transportStreamFiles)
-                {
-                    using (Stream transportStream = File.OpenRead(transportStreamFile))
-                    {
-                        transportStream.CopyTo(outputStream);
-                    }
-                }
-            }
-
-            return transportStreamFiles;
-        }
-
-        static List<string> downloadFiles(List<string> transportStreamFiles)
-        {
-            List<string> downloadedTransportStreamFiles = new List<string>();
-
-            using (var webClient = new WebClient())
-            {
-                int count = 1;
-                decimal total = transportStreamFiles.Count;
-
-                foreach (string transportStreamFile in transportStreamFiles)
-                {
-                    string percent = (count / total).ToString("p");
-                    Console.Write("\r{0}", "".PadRight(60, ' '));
-                    Console.Write("\rDownloading file " + count + " of " + total + ". (" + percent + ")");
-
-                    string tempFile = Path.GetTempFileName();
-                    webClient.DownloadFile(transportStreamFile, tempFile);
-                    downloadedTransportStreamFiles.Add(tempFile);
-
-                    count++;
-                }
-            }
-
-            return downloadedTransportStreamFiles;
-        }
-
-        static void deleteFiles(List<string> transportStreamFiles)
-        {
-            foreach (string transportStreamFile in transportStreamFiles)
-            {
-                File.Delete(transportStreamFile);
             }
         }
     }
